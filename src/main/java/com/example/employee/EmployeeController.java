d0684786c0e6fd09805423ced2c089181b942372
swathiraj <swathiraj687@gmail.com>
2023-12-01 12:57:43 +0000

    solution

---
+++ src/main/java/com/example/employee/EmployeeController.java
@@ -0,0 +1,45 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.*;
+ * 
+ */
+
+// Write your code here
+package com.example.employee;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import com.example.employee.EmployeeService;
+import com.example.employee.Employee;
+
+@RestController
+public class EmployeeController {
+    EmployeeService employeeService = new EmployeeService();
+
+    @GetMapping("/employees")
+    public ArrayList<Employee> getAllEMployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable("employeeId") int employeeId) {
+        return employeeService.getEmployeeById(employeeId);
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
+
+    @PutMapping("/employees/{employeeId}")
+    public Employee updatEmployee(@PathVariable("employeeId") int employeeId, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(employeeId, employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void deleteEmployee(@PathVariable("employeeId") int employeeId) {
+        employeeService.deleteEmployee(employeeId);
+    }
+}
---
+++ src/main/java/com/example/employee/EmployeeRepository.java
@@ -0,0 +1,16 @@
+// Write your code here
+package com.example.employee;
+
+import java.util.*;
+
+public interface EmployeeRepository {
+    ArrayList<Employee> getAllEmployees();
+
+    Employee getEmployeeById(int employeeId);
+
+    Employee addEmployee(Employee employee);
+
+    Employee updateEmployee(int employeeId, Employee employee);
+
+    void deleteEmployee(int employeeId);
+}
